a0750c7618d713f5eac3c5dd01e8088a175a22ef
johan <github.manifesto500@passmail.net>
2025-10-31 00:01:04 +0100

    6th commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-            SecurityHeaders::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
